8e682b9d19a3165acdbd6bd4a2591e85c395ed18
Robert <robert.balicki@gmail.com>
2018-06-23 15:14:06 -0400

    v1 - make a match_html_token macro that always fails
    * use the alt! combinator to hook it up to match_dom_element, match_string
      and match_group, each of which always fails.
    * add logging to the entry point of the macro library.

--- README.md
+++ README.md
@@ -10,5 +10,9 @@ Follow along with the [presentation](https://docs.google.com/presentation/d/11KK
 
 Bare-bones procedural macro that does nothing to the input, and the basic types we will be using.
 
+* v1
 
+Create a `match_html_token` macro that uses `alt!` to match on a group, a DomElement or a string.
+Hook each of these up to a an always-failing function, for now.
 
+In the macros entrypoint, add logging of the input and final result.
--- jsx_macro/src/lib.rs
+++ jsx_macro/src/lib.rs
@@ -3,10 +3,22 @@
 extern crate jsx_types;
 extern crate proc_macro;
 extern crate proc_macro2;
+#[macro_use]
+extern crate nom;
+// #[macro_use]
+extern crate quote;
 
-use proc_macro::TokenStream;
+mod parsers;
+
+use proc_macro::{TokenStream,TokenTree};
 
 #[proc_macro]
 pub fn jsx(input: TokenStream) -> TokenStream {
-  input
+  println!("\nInput = {:?}\n", input);
+  let vec_of_trees: Vec<TokenTree> = input.into_iter().collect();
+
+  let parsed = parsers::match_html_token(&vec_of_trees);
+  println!("Output = {:?}\n", parsed);
+
+  parsed.unwrap().1
 }
