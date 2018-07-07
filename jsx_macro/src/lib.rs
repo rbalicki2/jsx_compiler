922deda2c8f8ee8f12962dea95ed6da9e84f1c0d
Robert <robert.balicki@gmail.com>
2018-07-07 00:08:45 -0400

    add verbose mode

--- jsx_macro/src/lib.rs
+++ jsx_macro/src/lib.rs
@@ -12,6 +12,22 @@ mod parsers;
 
 #[proc_macro]
 pub fn jsx(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+  let input_2: proc_macro2::TokenStream = input.into();
+  let vec_of_trees: Vec<proc_macro2::TokenTree> = input_2.into_iter().collect();
+
+  let parsed = parsers::match_jsx(&vec_of_trees);
+  let unwrapped = parsed.unwrap();
+  let remaining = unwrapped.0;
+
+  if remaining.len() > 0 {
+    panic!("the jsx! macro had left over characters. Make sure you only pass one html node.");
+  }
+
+  unwrapped.1.into()
+}
+
+#[proc_macro]
+pub fn jsx_verbose(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   println!("\nInput = {:?}\n", input);
   let input_2: proc_macro2::TokenStream = input.into();
   let vec_of_trees: Vec<proc_macro2::TokenTree> = input_2.into_iter().collect();
